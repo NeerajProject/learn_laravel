a011abc1a8125d9c586b674d5888a7d9ad2f892a
Neeraj Krishnan V M <neeraj.krishnan@appingtechnology.com>
2025-10-18 15:43:44 +0530

    added

--- e_comerce/resources/js/pages/account/create.tsx
+++ e_comerce/resources/js/pages/account/create.tsx
@@ -4,6 +4,7 @@ import AppLayout from '@/layouts/app-layout';
 import { dashboard } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
+import { useForm, Link } from '@inertiajs/react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -12,7 +13,20 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-export default function Dashboard() {
+export default function Create() {
+    const { data, setData, post, processing, errors } = useForm({
+        name: '',
+        code: '',
+        type: '',
+        sub_type: '',
+     });
+
+    const handleSubmit = (e) => {
+     e.preventDefault();
+     post(route('account.store'),data);
+  };
+
+    
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Account" />
--- e_comerce/resources/js/pages/account/index.tsx
+++ e_comerce/resources/js/pages/account/index.tsx
@@ -24,6 +24,9 @@ export default function Dashboard() {
                   </div>
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
              
+             <div>
+                
+             </div>
             </div>
         </AppLayout>
     );

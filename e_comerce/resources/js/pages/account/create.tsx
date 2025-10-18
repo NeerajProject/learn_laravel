e012c917cf160e9ea2cee00d648bca0d5303ea56
Neeraj Krishnan V M <neeraj.krishnan@appingtechnology.com>
2025-10-18 16:32:25 +0530

    added created view

--- e_comerce/resources/js/pages/account/create.tsx
+++ e_comerce/resources/js/pages/account/create.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
+import { SelectGroup } from '@/components/ui/select';
 import AppLayout from '@/layouts/app-layout';
 import { dashboard } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
@@ -15,52 +16,52 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 
 export default function Create() {
-//     const { data, setData, post, processing, errors } = useForm({
-//         name: '',
-//         code: '',
-//         type: '',
-//         sub_type: '',
-//      });
 
-//     const handleSubmit = (e) => {
-//      e.preventDefault();
-//      post(route('account.store'),data);
-//   };
-
-    
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Account" />
-         
-       <form className='space-y-4'>
-            <div className='m-4'>
-                <Button type="submit">Save</Button>
-            </div>
-            <div className="w-8/12 p-4 space-y-4">
+
+            <form className='space-y-4'>
+                <div className='m-4'>
+                    <Button type="submit">Save</Button>
+                </div>
+                <div className="w-8/12 p-4 space-y-4">
 
 
 
-<div className="gap-1.5">
-<Label>Name</Label>
-<Input placeholder="Product Name"></Input>
+                    <div className="gap-1.5">
+                        <Label>Name</Label>
+                        <Input placeholder="Account Name"></Input>
+                    </div>
+                    <div className="gap-1.5">
+                        <Label>Code</Label>
+                        <Input placeholder="Code" ></Input>
+                    </div>
+                   <div className="gap-1.5">
+  <label htmlFor="type" className="mdc-floating-label">
+    Account Type
+  </label>
+  <div>
+  <select>
+    <option value="">Select Type</option>
+    <option value="Asset">Asset</option>
+    <option value="Liability">Liability</option>
+    <option value="Equity">Equity</option>
+    <option value="Revenue">Revenue</option>
+    <option value="Expense">Expense</option>
+  </select>
 </div>
-<div className="gap-1.5">
-<Label>Price</Label>
-<Input placeholder="Price" ></Input>
 </div>
-<div className="gap-1.5">
-<Label >Description</Label> 
+
 </div>
-            </div>
-           
-                        </form>
-        
-           
+            </form>
 
 
 
-      
-    
+
+
+
+
 
         </AppLayout>
     );
